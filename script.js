43e0ec922e20ea6df3a47a7297120f0e36adf187
Chambrin <achambrin@outlook.fr>
2021-11-07 18:52:17 +0100

    partie1

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-vue new {
-    
-}
+const vm = new Vue ({
+    el: '#app',
+
+    data: {
+        hautGauche: false,
+        hautDroite: false,
+        basGauche: false,
+        basDroite: false,
+        sequence: [],
+        squareMapping: ['hautGauche','hautDroite',' basGauche','basDroite'],
+
+    },
+
+    methods: {
+        addNewElemToSequence() {
+            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+        },
+        newGame() {
+            this.addNewElemToSequence();
+        },
+    }
+
+})
