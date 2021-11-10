bb1a0694a9a931a30b69ba1aa6a85e3b42c4f45b
Chambrin <achambrin@outlook.fr>
2021-11-10 13:03:17 +0100

    upgrade

--- script.js
+++ script.js
@@ -7,17 +7,61 @@ const vm = new Vue ({
         basGauche: false,
         basDroite: false,
         sequence: [],
+        tmp: [],
         squareMapping: ['hautGauche','hautDroite',' basGauche','basDroite'],
-
     },
-
     methods: {
         addNewElemToSequence() {
             this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)])
+            this.tmp = this.sequence.slice();
         },
-        newGame() {
-            this.addNewElemToSequence();
+        allGray() {
+                this.hautGauche = false;
+                this.hautDroite = false;
+                this.basGauche = false;
+                this.basDroite = false;
         },
-    }
 
+        newGame() {
+            this.sequence = [];
+            this.nextTurn();
+        },
+        nextTurn() {
+            this.addNewElemToSequence();
+            this.allGray();
+            this.playSequence(this.tmp[0]);
+        },
+        playSequence: function (instruction) {
+            this[instruction] = true;
+            setTimeout(function(){
+                vm.allGray();
+                vm.tmp.shift();
+                if (vm.tmp[0]) {
+                    setTimeout(function(){
+                    vm.playSequence(vm.tmp[0]);
+                        }, 400
+                    );
+                    vm.playSequence(vm.tmp[0]);
+                }
+                else {
+                    vm.tmp = vm.sequence.slice();
+                }
+            }, 400);
+        },
+        selectSquare(instruction) {
+            console.log(instruction)
+            if (instruction === this.tmp[0]) {
+                this[instruction] = true;
+                setTimeout(function() {
+                    vm.allGray();
+                    vm.tmp.shift();
+                    if(!vm.tmp[0]) {
+                        vm.nextTurn();
+                    }
+                }, 400);
+            } else {
+                alert('perdu')
+            }
+        }
+    }
 })
